6cf29959c24773c561308836c5c9425dbc002fed
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-10-06 19:06:22 +0900

    ユーザー登録の画面の部分を作成

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>ユーザー登録</h1>
+<form>
+    <input name="email"  type="email" placeholder="eメール">
+    <input name="password" type="password" placeholder="パスワード">
+    <input name="rePassword" type="password" placeholder="パスワード再入力">
+    <input type="submit" value="送信">
+</form>
+<a href="mypage.php">マイページへ</a>
+</body>
+</html>
