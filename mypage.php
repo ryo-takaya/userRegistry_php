7fe8b2b65711cec8f8ffe1345d14484f52b5944e
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-10-06 19:10:24 +0900

    マイページの画面を作成

---
+++ mypage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>登録画面</title>
+</head>
+<body>
+<h1>ユーザーページ</h1>
+<section>
+    <p>
+        あなたのemailは####
+    </p>
+    <p>
+        あなたの名前は@@@
+    </p>
+</section>
+
+<a href="index.php">ユーザー登録画面へ</a>
+</body>
+</html>
