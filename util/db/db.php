ba8d0ac4b3888341abd740b1943e6a9b6f4de5fd
ryoutaka <ryoutakaya3623@yahoo.co.jp>
2021-10-10 17:37:05 +0900

    db接続クラスを作成

---
+++ util/db/db.php
@@ -0,0 +1,20 @@
+<?php
+
+class SettingDb {
+    /**
+     * @return PDO dbハンドラを返す
+     * @throws PDOException データベースの接続に失敗したらthrow
+     */
+    public static function connectDb() {
+        try{
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
+            ];
+            return new PDO('mysql:dbname=uers;host=localhost;charset=utf8','root','root',$options);
+        } catch(PDOException $exception){
+            throw new RUNtimeException('dbの接続に失敗しました');
+        }
+    }
+}
